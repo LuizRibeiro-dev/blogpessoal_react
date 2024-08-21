5605bfc6c12b57eed1ad6b6d95c8dafccf81c14d
Luiz Ribeiro <luizribeirodesenvolvedor@gmail.com>
2024-08-21 12:10:30 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from "./pages/home/Home"
+
+function App() {
+//codigo TS
+  return (
+    // codigo tsx => html + css
+    <>
+    <Home/>
+   
+  </>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,36 @@
+
+function Home() {
+  return (
+    <div style={{
+        width: "100vw", 
+        display: "flex",
+        justifyContent: "center"
+    }}>
+        <div>
+            <div style={{
+                width: "80vw",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}>
+                <h2>Seja bem-vindo!</h2>
+                <p>Expresse aqui seus pensamentos e opiniões</p>
+            </div>
+            <div style={{
+                width: "80vw",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}>
+            <img 
+                src="https://i.imgur.com/VpwApCU.png"
+                alt="Imagem da pagina home"
+                width="400px"
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
